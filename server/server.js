5065c22dbcf62e87847a46a596711c53aa7fe2c6
somnathbm <somnathbm0@gmail.com>
2016-08-31 19:48:33 +0530

    test data to bluemix #4

--- server/server.js
+++ server/server.js
@@ -105,23 +105,8 @@ app.use(function(req, res, next) {
 // connect to the IBM Watson IoT platform
 appClient.connect();
 
+console.log('App connected!');
 appClient.log.setLevel = 'info';
-
-// device connect event
-appClient.on('connect', function(){
-    console.log('App connected!');
-
-    var myData = { 'name': 'somnath', 'role': 'dev' };
-
-    // subscribe to device events
-    appClient.subscribeToDeviceEvents("status");
-    // subscribe to device status
-    appClient.subscribeToDeviceStatus("status");
-    // now, publishing event
-    setInterval(function(){
-        appClient.publishDeviceCommand("pi_cierra", "homePi", "status", "json", myData);
-    }, 1000);
-});
 
 // device response event
 appClient.on("deviceEvent", function(deviceType, deviceId, eventType, format, payload){
@@ -144,9 +129,24 @@ logger.info('mbaas context root: '+ibmconfig.getContextRoot());
 
 var successComm = function(){
     return function(req, res){
-        var response = "`" + req.body.L + "`";
+        var response = req.body.L;
         console.log(response);
         res.send("request received & processed");
+
+        // send over to IBM Bluemix
+        // device connect event
+        appClient.on('connect', function(){
+            var myData = { 'L': response };
+
+            // subscribe to device events
+            appClient.subscribeToDeviceEvents("status");
+            // subscribe to device status
+            appClient.subscribeToDeviceStatus("status");
+            // now, publishing event
+            setInterval(function(){
+                appClient.publishDeviceCommand("pi_cierra", "homePi", "status", "json", myData);
+            }, 1000);
+        });
     };
 };
 
